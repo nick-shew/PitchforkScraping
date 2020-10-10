848b78f682f2e594bba5b3c1b639f02bfb7115a3
nick-shew <nicholas.shew@gmail.com>
2020-10-10 16:55:18 -0500

    remove unused code in extensions

--- PitchforkScraper/Extensions.cs
+++ PitchforkScraper/Extensions.cs
@@ -8,33 +8,6 @@ namespace PitchforkScraping
 {
     static class Extensions
     {
-        //public static IEnumerable<HtmlNode> GetNodesWithClassDescendantSearch(this HtmlDocument doc, string className)
-        //{
-        //    return doc.DocumentNode.Descendants().Where(n => n.HasClass(className));
-        //}
-        //public static IEnumerable<HtmlNode> GetNodesWithClass(this HtmlDocument doc, string className, string divOrSpan = "div")
-        //{
-        //    var nodes = doc.DocumentNode.SelectNodes($"//{divOrSpan}[@class='{className}']");//TODO--why does this work only some of the time
-        //    if (nodes == null)
-        //    {
-        //        return doc.GetNodesWithClassDescendantSearch(className);
-        //    }
-        //    else
-        //    {
-        //        return nodes.ToList();
-        //    }
-        //}
-        //public static bool TryGetTextOfFirstNodeWithClass(this HtmlDocument doc, string className, string divOrSpan = "div", out string text)
-        //{
-        //    try
-        //    {
-        //        text = doc.GetNodesWithClass(className, divOrSpan).FirstOrDefault().InnerText;
-        //        return true;
-        //    }catch (NullReferenceException){
-        //        text = "";
-        //        return false;
-        //    }
-        //}
         public static bool TryGetTextOfFirstNodeWithClass(this HtmlDocument doc, string searchString, out string text)
         {
             try
